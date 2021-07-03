405ca30cf4ed30dd5592d87c766a110f5f8ba570
chriselrod <elrodc@gmail.com>
2021-07-02 22:21:30 -0400

    Try adjusting tol again

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,16 @@ using TriangularSolve, LinearAlgebra
 using Test
 
 function test_solve(::Type{T}) where {T}
+  rtol = T === Float32 ? 1e-2 : sqrt(eps(T))
   for n ∈ 1:100
     for m ∈ max(1,n-10):n+10
       A = rand(T, m, n); res = similar(A);
       B = rand(T, n, n);      
-      @test TriangularSolve.rdiv!(res, A, UpperTriangular(B)) ≈ A / UpperTriangular(B) rtol=sqrt(eps(T))
-      @test TriangularSolve.rdiv!(res, A, UnitUpperTriangular(B)) ≈ A / UnitUpperTriangular(B) rtol=sqrt(eps(T))
+      @test TriangularSolve.rdiv!(res, A, UpperTriangular(B)) ≈ A / UpperTriangular(B) rtol=rtol
+      @test TriangularSolve.rdiv!(res, A, UnitUpperTriangular(B)) ≈ A / UnitUpperTriangular(B) rtol=rtol
       A = rand(T, n, m); res = similar(A);
-      @test TriangularSolve.ldiv!(res, LowerTriangular(B), A) ≈ LowerTriangular(B) \ A rtol=sqrt(eps(T))
-      @test TriangularSolve.ldiv!(res, UnitLowerTriangular(B), A) ≈ UnitLowerTriangular(B) \ A rtol=sqrt(eps(T))
+      @test TriangularSolve.ldiv!(res, LowerTriangular(B), A) ≈ LowerTriangular(B) \ A rtol=rtol
+      @test TriangularSolve.ldiv!(res, UnitLowerTriangular(B), A) ≈ UnitLowerTriangular(B) \ A rtol=rtol
     end
   end
 end
