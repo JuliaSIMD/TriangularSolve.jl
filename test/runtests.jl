927be34ddade7a89856d6591072229933477f5ed
Chris Elrod <elrodc@gmail.com>
2021-07-03 10:54:02 -0400

    + I for better conditioning on solves

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,15 @@ using TriangularSolve, LinearAlgebra
 using Test
 
 function test_solve(::Type{T}) where {T}
-  rtol = T === Float32 ? 1e-2 : sqrt(eps(T))
   for n ∈ 1:100
     for m ∈ max(1,n-10):n+10
       A = rand(T, m, n); res = similar(A);
-      B = rand(T, n, n);      
-      @test TriangularSolve.rdiv!(res, A, UpperTriangular(B)) ≈ A / UpperTriangular(B) rtol=rtol
-      @test TriangularSolve.rdiv!(res, A, UnitUpperTriangular(B)) ≈ A / UnitUpperTriangular(B) rtol=rtol
+      B = rand(T, n, n) + I;
+      @test TriangularSolve.rdiv!(res, A, UpperTriangular(B)) * UpperTriangular(B) ≈ A
+      @test TriangularSolve.rdiv!(res, A, UnitUpperTriangular(B)) * UnitUpperTriangular(B) ≈ A
       A = rand(T, n, m); res = similar(A);
-      @test TriangularSolve.ldiv!(res, LowerTriangular(B), A) ≈ LowerTriangular(B) \ A rtol=rtol
-      @test TriangularSolve.ldiv!(res, UnitLowerTriangular(B), A) ≈ UnitLowerTriangular(B) \ A rtol=rtol
+      @test LowerTriangular(B) * TriangularSolve.ldiv!(res, LowerTriangular(B), A) ≈ A
+      @test UnitLowerTriangular(B) * TriangularSolve.ldiv!(res, UnitLowerTriangular(B), A) ≈ A
     end
   end
 end
