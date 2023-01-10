d3a851884aa89f1e3dad574f134125613db18df1
Chris Elrod <elrodc@gmail.com>
2023-01-10 18:00:49 -0500

    format

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,24 @@
 using TriangularSolve
 using Documenter
 
-DocMeta.setdocmeta!(TriangularSolve, :DocTestSetup, :(using TriangularSolve); recursive=true)
+DocMeta.setdocmeta!(
+  TriangularSolve,
+  :DocTestSetup,
+  :(using TriangularSolve);
+  recursive = true,
+)
 
 makedocs(;
-    modules=[TriangularSolve],
-    authors="chriselrod <elrodc@gmail.com> and contributors",
-    repo="https://github.com/JuliaSIMD/TriangularSolve.jl/blob/{commit}{path}#{line}",
-    sitename="TriangularSolve.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://JuliaSIMD.github.io/TriangularSolve.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+  modules = [TriangularSolve],
+  authors = "chriselrod <elrodc@gmail.com> and contributors",
+  repo = "https://github.com/JuliaSIMD/TriangularSolve.jl/blob/{commit}{path}#{line}",
+  sitename = "TriangularSolve.jl",
+  format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical = "https://JuliaSIMD.github.io/TriangularSolve.jl",
+    assets = String[],
+  ),
+  pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/JuliaSIMD/TriangularSolve.jl",
-)
+deploydocs(; repo = "github.com/JuliaSIMD/TriangularSolve.jl")
